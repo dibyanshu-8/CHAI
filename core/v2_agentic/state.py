ba9f5b7df27e05e995f43f8c85affdcbe193ebc2
Dibyanshu kar <dibyanshukar8@gmail.com>
2026-06-22 00:05:27 +0530

    fixing some issues

--- core/v2_agentic/state.py
+++ core/v2_agentic/state.py
@@ -1,10 +1,16 @@
-#Agent's shared memory
-from typing import TypedDict,List,Annotated
+# Agent's shared memory
+from typing import TypedDict, List, Annotated
 import operator
 
 class AgentState(TypedDict):
-    raw_news:List[str]
-    supplier_info:dict
-    identified_risks:List[dict]
-    final_alert:str
-    logs:Annotated[List[str],operator.add]
+    raw_news: List[str]
+    supplier_info: dict
+    identified_risks: List[dict]
+    final_alert: str
+    logs: Annotated[List[str], operator.add]
+    
+    # Autonomous routing variables
+    search_count: Annotated[int, operator.add] 
+    
+    
+    is_data_sufficient: bool
